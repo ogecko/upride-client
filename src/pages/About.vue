8f05afeb3bcf52251b566d88a84bb8c45efc1567
David Morrison <jdmorriso@gmail.com>
2018-09-07 16:53:50 +1000

    enh(about): add more info on about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -11,14 +11,23 @@
     </div>
     <div v-else>
       <h2 class="text-align-center text-weight-bold">UpRide.com.au</h2>
-      <p class="text-align-center q-mt-md">
+      <p class="text-align-center q-my-lg">
         UpRide is the most convenient way to recharge and refresh while in your rideshare vehicle. 
         Simply purchase what you desire from the online shop, and your driver will hand it to you when safe to do so.
       </p>
-      <div class="row justify-center">
-        <div class="col-2">Version</div>
-        <div class="col-2">1.0.1</div>
+      <div class="row q-my-md justify-center items-center">
+        <div class="col-4">UpRide Version</div>
+        <div class="col-3">1.0.1</div>
       </div>
+      <div class="row q-my-md justify-center items-center">
+        <div class="col-4">Number of Products</div>
+        <div class="col-3">{{$store.state.products.length}}</div>
+      </div>
+      <div class="row q-my-md justify-center items-center">
+        <div class="col-4">Your Driver</div>
+        <div class="col-3">{{$store.state.driver.name}}</div>
+      </div>
+
     </div>
   </q-page>
 </template>
