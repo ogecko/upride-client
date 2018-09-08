71d718f3fd22b71c0c1641032a28b09f1b5d49cb
David Morrison <jdmorriso@gmail.com>
2018-09-08 10:18:52 +1000

    enh(pwa): add progressive web app capabilities

--- quasar.conf.js
+++ quasar.conf.js
@@ -86,39 +86,39 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {},
       manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
+        name: 'UpRide.com.au',
+        short_name: 'UpRide',
+        description: 'UpRide is the most convenient way to recharge and refresh while in your rideshare vehicle.',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
         theme_color: '#027be3',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
+            'src': 'statics/icons/android-icon-72x72.png',
+            'sizes': '72x72',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/icons/android-icon-96x96.png',
+            'sizes': '96x96',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/android-icon-144x144.png',
+            'sizes': '144x144',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/android-icon-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
+            'src': 'statics/icons/ms-icon-310x310.png',
+            'sizes': '310x310',
             'type': 'image/png'
           },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
-          }
         ]
       }
     },
